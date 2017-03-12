6d89dd8e14469b966d17aba3e52d4ef1f55e5036
saurav sen <saurav.tech@gmail.com>
2017-03-11 21:53:52 -0600

    remove cookbooks

--- cookbooks/demo/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-
-# Attributes for JAVA
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk_version'] = '8'
-default['java']['oracle']['accept_oracle_download_terms'] = true
-default['java']['java_home'] = '/apps/install/java'
