428725d58d3e98f3dd0fe85d753eeba5eab245e5
Saurav Sen <ssen5@sapient.com>
2017-08-28 12:51:49 -0500

    added test cookbook

--- test/cookbooks/mytest/metadata.rb
+++ test/cookbooks/mytest/metadata.rb
@@ -1,11 +1,6 @@
-name             'mytest'
-maintainer       'Saurav'
-maintainer_email 'saurav.tech@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures demo'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-issues_url       'https://github.com/tonan/demo/issues'
-source_url       'https://github.com/tonan/demo'
+name              'mytest'
+license           'Apache 2.0'
+description       'This cookbook is used with test-kitchen to configure box for Target env'
+version           '1.0.0'
 
-depends "demo"
+depends 'demo'
